22ec59c665a7e955baa4c08368942c7229a2268a
Trevor <tlist403@gmail.com>
2021-09-01 16:17:25 -0300

    ClearAll now works with correct display

--- script.js
+++ script.js
@@ -17,52 +17,33 @@ document.addEventListener('keydown', function(event) {
     }
 
     if (parseKey >= 0 && parseKey <= 9) {
-        
-        
-       
-
-       
         if (operatorToOperate && numToOperate1) {
             numToOperate2 += parseInt(event.key, 10);
             topDisplay.textContent = `${numToOperate1} ${operatorToOperate} ${numToOperate2}`
-            // console.log(numToOperate1)
-            // console.log(numToOperate2)
         } else if (!numToOperate1 || !operatorToOperate) {
-            
             numToOperate1 += parseInt(event.key, 10); 
-            
-
             topDisplay.textContent = `${numToOperate1} ${operatorToOperate} ${numToOperate2}`
-            // console.log(numToOperate1)
-            // console.log(numToOperate2)
+           
         }
-
-
-       
-        operate(numToOperate1, operatorToOperate, numToOperate2);   
         console.log(`${numToOperate1} ${operatorToOperate} ${numToOperate2}`)
-
-        
-        /* First, we need to be able to store an input into the first variable if both num1 and num2 are null. 
-        Then, if the operator is not null, we want to store an operator in the operator variaable. If num1 is null, we should not store a value.
-        After this, we want to accept another number if num1 and operator are not null. 
-        Then, we want to store the value of num1 and num2 operation into num1 after we type another operator or the equals sign.
-        Finally, we need to do this in such a manner that we can backspace.*/
     }
     
     if (event.key == '.' || event.key == ',') {
         alert('Yep')
     }
-    operate();
+
+    if (event.key == 'Enter') {
+        operate(numToOperate1, operatorToOperate, numToOperate2);  
+   }
+
+   if (event.key == 'Backspace') {
+       clearAll();
+   }
+    
 });
 
 const operate = (num1, operator, num2) => {
-    clearAll();
-
-    
-
-    
-    
+   
     let output = topDisplay.textContent;
     output = num1 + operator + num2;
     
@@ -110,12 +91,12 @@ const divide = (a,b) => {
 }
 
 const clearAll = () => {
-    if(event.key == 'Backspace') {
-        numToOperate1 = '';
-        operatorToOperate = '';
-        numToOperate2 = '';
-        topDisplay.textContent = null; 
-        bottomDisplay.textContent = null;
-        console.log("Hurray!")
-    }
+    
+    numToOperate1 = '';
+    operatorToOperate = '';
+    numToOperate2 = '';
+    topDisplay.textContent = null; 
+    bottomDisplay.textContent = null;
+    console.log("Hurray!");
+    
 }
