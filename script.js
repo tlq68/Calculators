24c04ee0c24de1b6fb05ddcb9c8ffc6230168daf
Trevor <tlist403@gmail.com>
2021-09-01 17:12:21 -0300

    Each input updates new values properly

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const topDisplay = document.querySelector('#top-number');
 const bottomDisplay = document.querySelector('#bottom-number');
-let topStr = '';
 
 let numToOperate1 = '';
 let operatorToOperate = '';
@@ -47,19 +46,19 @@ const operate = (num1, operator, num2) => {
     let output = topDisplay.textContent;
     output = num1 + operator + num2;
     
-    if( operator == '+') {
+    if( operator == '+' && event.key == 'Enter') {
         
         bottomDisplay.textContent = add(num1, num2);
     }
-    if( operator == '-') {
+    if( operator == '-' && event.key == 'Enter') {
         
         bottomDisplay.textContent = subtract(num1, num2);
     }
-    if( operator == '*') {
+    if( operator == '*' && event.key == 'Enter') {
         
         bottomDisplay.textContent = multiply(num1, num2);
     }
-    if( operator == '/') {
+    if( operator == '/' && event.key == 'Enter') {
         
         bottomDisplay.textContent = divide(num1, num2);
     }
@@ -71,19 +70,32 @@ const add = (a,b) => {
     let parsedA = parseInt(a, 10);
     let parsedB = parseInt(b, 10);
 
+    numToOperate1 = parsedA + parsedB;
+    numToOperate2 = '';
+    topDisplay.textContent = numToOperate1;
+
     return parsedA + parsedB;
 }
 
 const subtract = (a,b) => {
+    numToOperate1 = a - b;
+    numToOperate2 = '';
+    topDisplay.textContent = numToOperate1;
     return a - b;
 }
 
 const multiply = (a,b) => {
+    numToOperate1 = a * b;
+    numToOperate2 = '';
+    topDisplay.textContent = numToOperate1;
     return a * b;
 }
 
 const divide = (a,b) => {
     if (b != 0) {
+        numToOperate1 = a / b;
+        numToOperate2 = '';
+        topDisplay.textContent = numToOperate1;
        return a / b;
     } else { 
         return 'Don\'t be silly!';
